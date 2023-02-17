1e4a59265aaeb80326233c7549849ed357e7aa83
kawagami <joelai1988@gmail.com>
2023-02-17 19:34:56 +0800

    add sweetalert2

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -13,15 +13,22 @@
 
 <body>
     @include('components.navbar')
-    @if ( session('error') )
-        <div class="w-100 d-flex flex-column align-items-center justify-content-center">
-            <h1 class="text-danger">{{ session('error') }}</h1>
-        </div>
-    @endif
 
     @yield('main')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    @if ( session('error') )
+        <script>
+            Swal.fire(
+            "{{session('error')}}",
+            '',
+            'error'
+            )
+        </script>
+    @endif
+
     @yield('js')
 </body>
 
