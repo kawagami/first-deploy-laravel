a8209eb95a66a585225e7bbd692bbe8e671b561d
kawagami <joelai1988@gmail.com>
2023-03-07 22:06:13 +0800

    [ FIX ] 修改 if 寫法 v2

--- app/Services/Socialite/BaseService.php
+++ app/Services/Socialite/BaseService.php
@@ -42,8 +42,32 @@ class BaseService
         ];
 
         // 檢查 email 是否存在
-        $auth_user = $this->repository->user_email_is_exists($user->email);
-        if (!$auth_user) {
+        if ($auth_user = $this->repository->user_email_is_exists($user->email)) {
+            // 檢查是否有相同的 auth_type
+            if ($same_auth_type = $this->repository->user_has_same_auth_type($auth_user, $this->auth_name)) {
+                // 檢查該認證 ID 是否相同
+                if ($same_auth_type->auth_id == $user->id) {
+                    // 是
+                    // 更新 token 之類的東西
+                    // 登入
+                    Auth::login($auth_user);
+                } else {
+                    // 否 會有這狀況嗎?
+                    info(json_encode($user));
+                    info(json_encode($same_auth_type));
+                    throw new \Exception('did this happen?');
+                }
+            } else {
+                // 新增 auth
+                $auth_data = $auth_data_without_user_id;
+                $auth_data["user_id"] = $auth_user->id;
+                $new_auth = $this->repository->create_auth($auth_data);
+
+                // user 新增新的 auth
+                $this->repository->user_add_auth($auth_user, $new_auth);
+                Auth::login($auth_user);
+            }
+        } else {
             $user_data = [
                 'name'     => $user->name ?? $user->nickname ?? $user->email,
                 'email'    => $user->email,
@@ -61,33 +85,6 @@ class BaseService
             // 子表新增 auth_type & 該認證 ID
             $this->repository->user_add_auth($new_user, $new_auth);
             Auth::login($new_user);
-        }
-
-        // 檢查是否有相同的 auth_type
-        $same_auth_type = $this->repository->user_has_same_auth_type($auth_user, $this->auth_name);
-        if (!$same_auth_type) {
-
-            // 新增 auth
-            $auth_data = $auth_data_without_user_id;
-            $auth_data["user_id"] = $auth_user->id;
-            $new_auth = $this->repository->create_auth($auth_data);
-
-            // user 新增新的 auth
-            $this->repository->user_add_auth($auth_user, $new_auth);
-            Auth::login($auth_user);
-        }
-
-        // 檢查該認證 ID 是否相同
-        if ($same_auth_type->auth_id == $user->id) {
-            // 是
-            // 更新 token 之類的東西
-            // 登入
-            Auth::login($auth_user);
-        } else {
-            // 否 會有這狀況嗎?
-            info(json_encode($user));
-            info(json_encode($same_auth_type));
-            throw new \Exception('did this happen?');
         }
 
         return redirect(route('index'));
