056abdddf547ee1fb37aa796d6010141d1dd2605
kawagami <joelai1988@gmail.com>
2023-05-08 23:30:56 +0800

    [ UPDATE ] img add lazy load

--- resources/views/components/irys.blade.php
+++ resources/views/components/irys.blade.php
@@ -1,5 +1,5 @@
 <div class="w-100 d-flex flex-column align-items-center justify-content-center">
-    <img id="irys" class="bg-secondary" src="https://i.imgur.com/hlkWrJn.png" alt="" srcset="">
+    <img id="irys" loading="lazy" class="bg-secondary" src="https://i.imgur.com/hlkWrJn.png" alt="" srcset="">
     <h1 class="justify-content-center">@lang('main.irys.title')</h1>
     <h2 class="justify-content-center">@lang('main.irys.descript')</h2>
 </div>
