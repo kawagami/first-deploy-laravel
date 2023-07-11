3f2d6bb4b4b24eac51653b15005337a1afb70c73
kawagami <joelai1988@gmail.com>
2023-07-11 22:59:27 +0800

    [ UPDATE ] 更新 navbar 資料

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -10,6 +10,9 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
+                    <a class="nav-link" href="https://vue.kawa.homes/">預計之後遷移過去的 vue 版本建置中頁面</a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" href="{{ route('note') }}">@lang('navbar.my-note')</a>
                 </li>
                 <li class="nav-item">
