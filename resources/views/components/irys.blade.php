d955b7b7cc3e5f9d8e2a7bccc8f9b72dddae05fe
kawagami <joelai1988@gmail.com>
2023-10-14 09:47:15 +0800

    update: 修改首頁訊息

--- resources/views/components/irys.blade.php
+++ resources/views/components/irys.blade.php
@@ -1,5 +1,4 @@
 <div class="w-100 d-flex flex-column align-items-center justify-content-center">
     <img id="irys" loading="lazy" class="bg-secondary" src="https://i.imgur.com/hlkWrJn.png" alt="" srcset="">
     <h1 class="justify-content-center">@lang('main.irys.title')</h1>
-    <h2 class="justify-content-center">@lang('main.irys.descript')</h2>
 </div>
