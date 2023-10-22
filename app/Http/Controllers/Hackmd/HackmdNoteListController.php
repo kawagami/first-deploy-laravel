2fe5b1de9e9bf32fb694ee40e854c632ab127826
kawagami <joelai1988@gmail.com>
2023-10-22 22:32:19 +0800

    update: 重寫 note 頁面的過濾邏輯

--- app/Http/Controllers/Hackmd/HackmdNoteListController.php
+++ app/Http/Controllers/Hackmd/HackmdNoteListController.php
@@ -19,42 +19,37 @@ class HackmdNoteListController extends Controller
      */
     public function index(Request $request)
     {
-
         $note_guest_can_read = HackmdNoteList::with('tags')
             ->where('readPermission', 'guest')
-            // ->orderByDesc('lastChangedAt')
-            ->orderByDesc('createdAt')
-            ->get();
+            ->orderByDesc('createdAt');
 
         // 過濾有"工作" tag 的筆記
-        $note_except_work = $note_guest_can_read->filter(function ($q) {
-            return !$q->tags()->where('name', '工作')->exists();
+        $note_guest_can_read->whereHas('tags', function ($query) {
+            $query->where('name', '<>', '工作');
         });
 
         // 取得條件的 note
         if ($request->has('tag') && $request->get('tag') !== 'all') {
-            // dd($note_except_work);
-            $note_except_work = $note_except_work->filter(function ($q) use ($request) {
-                return $q->tags()
-                    ->where('hackmd_tags.id', '=', $request->get('tag'))
-                    ->exists();
+            $tag_id = $request->get('tag');
+            $note_guest_can_read->whereHas('tags', function ($query) use ($tag_id) {
+                $query->where(DB::raw('"hackmd_tags"."id"'), $tag_id);
             });
-            // dd($note_except_work);
         }
 
+        $notes = $note_guest_can_read->get();
+
         // 轉換日期
-        $note_except_work = $note_except_work->map(function ($d) {
-            $d->createdAt = Carbon::parse(($d->createdAt / 1000))->toDateTimeString();
+        $notes = $notes->map(function ($d) {
+            $d->createdAt     = Carbon::parse(($d->createdAt / 1000))->toDateTimeString();
             $d->lastChangedAt = Carbon::parse(($d->lastChangedAt / 1000))->toDateTimeString();
+
             return $d;
         });
 
-        $data = [
-            'notes' => $note_except_work,
-            'tags' => HackmdTag::get(),
-        ];
-        //
-        return view('note', $data);
+        return view('note', [
+            'notes' => $notes,
+            'tags'  => HackmdTag::get(),
+        ]);
     }
 
     public static function get_notes_list(): string|null
