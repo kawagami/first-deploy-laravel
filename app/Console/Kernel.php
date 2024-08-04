ffad8e77bbc54c8ab8311669538181435ca98c92
kawagami <joelai1988@gmail.com>
2024-08-04 22:18:09 +0800

    update: schedule 測試有效 改成每天一次

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,11 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command("hackmd:get-note-lists")->daily();
-        $schedule->command("hackmd:get-note-lists")->everyMinute();
-        // $schedule->call(function () {
-        //     info(now()->toDateTimeString());
-        // })->everyMinute();
+        $schedule->command("hackmd:get-note-lists")->daily();
     }
 
     /**
